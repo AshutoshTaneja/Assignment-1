dc64e608af885d1e407824867310872ff8a0df11
AshutoshTaneja <96680121+AshutoshTaneja@users.noreply.github.com>
2022-01-11 15:27:35 +0530

    Add files via upload

--- output.py
+++ output.py
@@ -1,7 +1,7 @@
 Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-= RESTART: C:\Users\ashut\PEC\StudyMaterial\1st_Semester\ESC1_(Computing)\Assignments\asg1_EE_21104033.py
+================== RESTART: C:\Users\ashut\asg1_EE_21104033.py =================
 
 question 1
 Enter the first number: 2
