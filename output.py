73f805e1fb77dbe0622f7b648d777d26706ad356
AshutoshTaneja <96680121+AshutoshTaneja@users.noreply.github.com>
2022-01-11 15:21:33 +0530

    Assignment Uploaded

---
+++ output.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\ashut\PEC\StudyMaterial\1st_Semester\ESC1_(Computing)\Assignments\asg1_EE_21104033.py
+
+question 1
+Enter the first number: 2
+Enter the second number: 3
+Enter the third number: 4
+The Average of the numbers entered is: 3.00000
+
+question 2
+What is your Gross Income(to the nearest penny): 32000.67
+What are the total number of dependents: 4
+The income tax to be paid by you is:  2000.1339999999998
+
+question 3
+Hello Student!
+Enter your SID: 21104033
+Enter your Name: Ashutosh Taneja
+Enter your Gender(F,M,U(For Unknown)): M
+Enter the Course Name: Intro To Computing
+Enter your CGPA: 8.6
+[21104033, 'Ashutosh Taneja', 'M', 'Intro To Computing', 8.6]
+
+question 4
+Enter the marks of all the students: 12 13 14 11 11
+[11.0, 11.0, 12.0, 13.0, 14.0]
+
+question 5a
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+question 5b
+['Red', 'Green', 'White', 'Purple', 'Yellow']
